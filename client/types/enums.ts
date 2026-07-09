7c9b9c9935ad7a0be86663b1e03fc1162d3dfa3f
Waozi <waozi@proton.me>
2026-07-09 12:42:41 -0300

    Add Kick live stream support

--- client/types/enums.ts
+++ client/types/enums.ts
@@ -109,6 +109,7 @@ export enum PageRecipientVariant {
 export enum LiveStreamPlatformEnum {
   YOUTUBE = "youtube",
   TWITCH = "twitch",
+  KICK = "kick",
   X = "x",
   RUMBLE = "rumble",
   PEERTUBE = "peertube",
