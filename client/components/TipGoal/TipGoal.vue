08d28eecc8bc65924a164c2c60570640ae0bfdcf
sa8ab <59901734+sa8ab@users.noreply.github.com>
2026-07-19 03:48:09 +0330

    Update tip goal on tip page

--- client/components/TipGoal/TipGoal.vue
+++ client/components/TipGoal/TipGoal.vue
@@ -18,22 +18,38 @@ const { data } = useLazyAsyncData(
   },
   { server: false },
 );
+
+const percentage = computed(() => {
+  const amount = Number(data.value?.tipGoal.amount) ?? 0;
+  const tipsAmount = Number(data.value?.tipsAmount) ?? 0;
+  if (!tipsAmount) return 0;
+  return (amount / tipsAmount) * 100;
+});
 </script>
 
 <template>
-  <div class="">
-    <div class="flex justify-between">
-      <span></span>
-      <span class="">- XMR</span>
-    </div>
-    <div
-      class="w-full flex justify-center bg-background-2 rounded-full overflow-hidden h-4 relative"
-    >
+  <div>
+    <span>{{ data?.tipGoal.name }}</span>
+    <div class="flex flex-col gap-1">
+      <div class="flex justify-between">
+        <span></span>
+        <span class="">{{ data?.tipGoal.amount }} XMR</span>
+      </div>
       <div
-        class="bg-primary h-full rounded-full absolute left-0 top-0 z-0"
-        style="width: 70%"
-      ></div>
-      <div class="text-xs text-white relative">0.001 XMR (2%)</div>
+        class="w-full flex justify-center bg-background-2 rounded-full overflow-hidden h-4 relative"
+      >
+        <div
+          class="bg-primary h-full rounded-full absolute left-0 top-0 z-0"
+          style="width: 10%"
+        ></div>
+        <div class="text-xs text-white relative">
+          {{ data?.tipsAmount }} XMR ({{ percentage }}%)
+        </div>
+      </div>
+      <div class="flex justify-between">
+        <span></span>
+        <span class="text-sm text-pale">X remaining</span>
+      </div>
     </div>
   </div>
 </template>
