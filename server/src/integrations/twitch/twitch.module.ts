c0d2fc12837d9dd1843e7f57747e5d19acdd1f34
sa8ab <59901734+sa8ab@users.noreply.github.com>
2026-08-10 03:53:15 +0330

    Add email reminder for updating twitch token

--- server/src/integrations/twitch/twitch.module.ts
+++ server/src/integrations/twitch/twitch.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TwitchService } from './twitch.service';
 import { HttpModule } from '@nestjs/axios';
 import { TwitchTokenService } from './twitch-token.service';
+import { QueuesModule } from 'src/queues/queues.module';
 
 @Module({
-  imports: [HttpModule.register({})],
+  imports: [HttpModule.register({}), QueuesModule],
   providers: [TwitchService, TwitchTokenService],
   exports: [TwitchService, TwitchTokenService],
 })
